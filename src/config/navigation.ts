cce3f129b729ea2368a3a6f1ef91dd2d70fb5dd7
Rito Rhymes <rito@ritovision.com>
2026-02-27 23:47:24 -0500

    add sidebar main menu

---
+++ src/config/navigation.ts
@@ -0,0 +1,33 @@
+export interface SocialLink {
+  label: string;
+  href: string;
+  iconSrc: string;
+}
+
+export const socialLinks: SocialLink[] = [
+  {
+    label: 'X',
+    href: 'https://x.com/fulldecent',
+    iconSrc: '/images/icons/socials/x-white.png',
+  },
+  {
+    label: 'YouTube',
+    href: 'https://www.youtube.com/@fulldecent',
+    iconSrc: '/images/icons/socials/youtube-white.png',
+  },
+  {
+    label: 'Twitch',
+    href: 'https://www.twitch.tv/fulldecent',
+    iconSrc: '/images/icons/socials/twitch-white.png',
+  },
+  {
+    label: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/fulldecent/',
+    iconSrc: '/images/icons/socials/linkedin-white.png',
+  },
+  {
+    label: 'GitHub',
+    href: 'https://github.com/fulldecent',
+    iconSrc: '/images/icons/socials/github-white.png',
+  },
+];
